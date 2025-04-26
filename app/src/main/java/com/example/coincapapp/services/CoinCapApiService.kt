79f8cb8624173d73282e1a45fe3138d07d2dc24f
camilus corp <camilus_corp@yahoo.es>
2025-04-25 19:45:07 -0500

    deber firebase 25-04-2025

--- app/src/main/java/com/example/coincapapp/services/CoinCapApiService.kt
+++ app/src/main/java/com/example/coincapapp/services/CoinCapApiService.kt
@@ -11,7 +11,8 @@ class CoinCapApiService @Inject constructor (
     private val client: HttpClient
 ) {
     suspend fun  getAssets(): AssetsResponse {
-        val response: HttpResponse = client.get(urlString = "https://rest.coincap.io/v3/assets?apiKey=0e46f6021931e3999ce3675a3548da6eb0a895d3328a5c3cbdcc17e846dafc42")
+        val response: HttpResponse = client.get(urlString = "https://a27c2c52-aa18-451d-8b25-24fc7448c9f7.mock.pstmn.io/v3/assets")
+        //val response: HttpResponse = client.get(urlString = "https://rest.coincap.io/v3/assets?apiKey=0e46f6021931e3999ce3675a3548da6eb0a895d3328a5c3cbdcc17e846dafc42")
         return response.body()
     }
 }
